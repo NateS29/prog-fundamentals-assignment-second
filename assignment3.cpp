bdf8d163170b6d03dea8ea4694d1149ad19b2919
NateS29 <santiagojose697.sj@gmail.com>
2025-09-01 18:06:44 +0700

    Add files via upload

---
+++ assignment3.cpp
@@ -0,0 +1,11 @@
+//for second assignment with simulating assignemnt submission
+
+#include <stdio.h>
+
+int main(){
+	
+	printf("Hello! my name is Tan Santiago Jose Kurniawan, you may call me Tan\n");
+	printf("My favorite programming language right now is C...\nWith it being the only one I know");
+	
+	return 0;
+}

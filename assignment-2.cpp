4e0632e4e463c15777a805092ada98d34ded3960
NateS29 <santiagojose697.sj@gmail.com>
2025-09-01 17:55:51 +0700

    Add files via upload

---
+++ assignment-2.cpp
@@ -0,0 +1,15 @@
+//for first area of rectangle, without user input
+
+#include <stdio.h>
+
+int main()
+{
+    int widht = 3;
+    int lenght = 5;
+    int area = widht*lenght;
+
+    printf ("area of rectangle = %d", area);
+
+    return 0;
+}
+
